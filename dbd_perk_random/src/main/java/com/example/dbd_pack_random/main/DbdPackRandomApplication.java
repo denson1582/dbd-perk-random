d4248a51509ab60bf0a83fafccde66497066bdea
denson <denson034@gmail.com>
2026-03-02 19:26:57 +0900

    Dockerfile add

--- dbd_perk_random/src/main/java/com/example/dbd_pack_random/main/DbdPackRandomApplication.java
+++ dbd_perk_random/src/main/java/com/example/dbd_pack_random/main/DbdPackRandomApplication.java
@@ -1,4 +1,4 @@
-package com.example.dbd_pack_random;
+package com.example.dbd_pack_random.main;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
